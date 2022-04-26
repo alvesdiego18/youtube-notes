f2078340a4b1e54a0e744837e9b4528f9579d59a
Luiz Correia <luiz@luizcorreia.eti.br>
2022-04-26 01:37:45 -0300

    feat: mock popup.html

--- background.js
+++ background.js
@@ -1,29 +1,25 @@
-let dbMark
-let objStoreMark
+let dbMark;
+let objStoreMark;
 
 function initDataMark() {
+  const dbName = "marksDB";
+  var request = indexedDB.open(dbName, 2);
 
-    const dbName = "marksDB";
-    var request = indexedDB.open(dbName, 2);
+  request.onerror = function (event) {
+    console.log("Erro onErro ");
+    console.log(event);
+  };
 
-    request.onerror = function (event) {
-        console.log('Erro onErro ')
-        console.log(event)
-    };
-
-    request.onupgradeneeded = function (event) {
-        dbMark = event.target.result;
-    };
+  request.onupgradeneeded = function (event) {
+    dbMark = event.target.result;
+  };
 }
 
 function addDataMark(markData) {
-
-    if (dbMark) {
-
-        objStoreMark = dbMark.createObjectStore("marks", { autoIncrement: true });
-        objStoreMark.add(JSON.stringify(markData));
-    }
-    else {
-        console.log('nothing')
-    }
+  if (dbMark) {
+    objStoreMark = dbMark.createObjectStore("marks", { autoIncrement: true });
+    objStoreMark.add(JSON.stringify(markData));
+  } else {
+    console.log("nothing");
+  }
 }
